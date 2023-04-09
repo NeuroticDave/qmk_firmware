520b316d4a9fb67230e0245fea18f1f2d404bc44
dave <dave>
2023-04-09 04:52:21 +0100

    woops

--- keyboards/keychron/k4/keymaps/iso/keymap.c
+++ keyboards/keychron/k4/keymaps/iso/keymap.c
@@ -1,16 +1,3 @@
-/*
-Copyright 2020 Dimitris Mantzouranis
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include QMK_KEYBOARD_H
 #ifdef BLUETOOTH_ENABLE
 #include "iton_bt.h"
@@ -21,6 +8,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _LAYER1 1
 #define _LAYER2 2
 #define _LAYER3 3
+#define BT_PRO1 BT_PROFILE1
 
 enum custom_keycodes {
     LAYER0 = SAFE_RANGE,
