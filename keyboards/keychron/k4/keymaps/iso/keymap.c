f4aecacee7c1d4d3632665fe1e751be3aaaa9a31
NeuroticDave <Smalldave87@gmail.com>
2023-08-05 15:26:14 +0100

    Update keymap.c

--- keyboards/keychron/k4/keymaps/iso/keymap.c
+++ keyboards/keychron/k4/keymaps/iso/keymap.c
@@ -11,11 +11,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include "k4.c"
+#include "k4.h"
+
 #include QMK_KEYBOARD_H
-#ifdef BLUETOOTH_ENABLE
 #include "iton_bt.h"
 #include "outputselect.h"
-#endif
 
 #define BT_PRO1 BT_PROFILE1
 #define BT_PRO2 BT_PROFILE2
@@ -50,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*  Row:        0          1          2          3        4        5        6         7        8        9          10         11         12         13         14         15         16         17         18     */
     [_BASE] = { {   KC_ESC,    KC_F1,     KC_F2,     KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_DEL,    KC_HOME,   KC_END,    KC_PGUP,   KC_PGDN,   RGB_MOD },
                 {   KC_GRV,    KC_1,      KC_2,      KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_NO,     KC_NUM,    KC_PSLS,   KC_PAST,   KC_PMNS },
-                {   KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_NO,     KC_NO,     KC_P7,     KC_P8,     KC_P9,     KC_PPLS },
-                {   KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,    KC_NO,     KC_P4,     KC_P5,     KC_P6,     KC_NO   },
+                {   KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_NO,     KC_NO,     KC_P7,     KC_P8,     KC_P9,     KC_PPLS },
+                {   KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,    KC_NO,     KC_P4,     KC_P5,     KC_P6,     KC_NO   },
                 {   KC_LSFT,   KC_NUBS,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_UP,     KC_P1,     KC_P2,     KC_P3,     KC_PENT },
                 {   KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   MO(_FL),   KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_P0,     KC_PDOT,   KC_NO   }
               },
@@ -70,13 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  				|     | LALT|  LGUI|                             |     |   |     |    |    |    |    |    |    |
  				+--------------------------------------------------------------------------+-------------------+
 */
-    /*  Row:        0          1          2          3        4        5        6         7        8        9          10         11         12         13         14         15         16         17         18     */
-    [_FL] =   { {   QK_BOOT,  KC_SCRL,  KC_PAUS,   KC_APP,  _______,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,      KC_VOLD,   KC_VOLU,   _______,  KC_PSCR,  _______,  _______,  KC_SLEP,  RGB_TOG },
-                {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,   _______,  _______,    KC_NO,  _______,  _______,  _______,  RGB_HUI },
-                {   _______,  BT_PRO1,  BT_PRO2,  BT_PRO3,  _______,  _______,  _______,  _______,  _______,  _______,   BT_PAIR,     _______,   _______,  _______,    KC_NO,  _______,  _______,  _______,  RGB_SPI },
-                {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,     KC_NO,  _______,    KC_NO,  _______,  _______,  _______,    KC_NO },
-                {   _______,    KC_NO,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,     KC_NO,  _______,  _______,  _______,  _______,  _______,  RGB_SAI },
-                {   _______,  KC_LALT,  KC_LGUI,    KC_NO,    KC_NO,    KC_NO,  _______,    KC_NO,    KC_NO,    KC_NO,   _______,     MO(_FL),   _______,  _______,  _______,  _______,  _______,  _______,    KC_NO }
+    /*  Row:        0         1         2          3        4         5         6         7         8         9         10            11         12        13         14       15        16        17        18     */
+    [_FL] =   { {   QK_BOOT,  KC_SCRL,  KC_PAUS,  KC_APP,   _______,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,   KC_MUTE,     KC_VOLD,   KC_VOLU,  _______,   KC_PSCR, _______,  _______,  KC_SLEP,  RGB_TOG },
+                {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,   _______,  _______,   KC_NO,   _______,  _______,  _______,  RGB_HUI },
+                {   _______,  BT_PRO1,  BT_PRO2,  BT_PRO3,  _______,  _______,  _______,  _______,  _______,  _______,   BT_PAIR,     _______,   _______,  _______,   KC_NO,   _______,  _______,  _______,  RGB_SPI },
+                {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,   KC_NO,    _______,   KC_NO,   _______,  _______,  _______,    KC_NO },
+                {   _______,  KC_NO,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,     _______,   KC_NO,    _______,  _______,  _______,  _______,  _______,  RGB_SAI },
+                {   _______,  KC_LALT,  KC_LGUI,  KC_NO,    KC_NO,    KC_NO,    _______,  KC_NO,    KC_NO,    KC_NO,     _______,     MO(_FL),   _______,  _______,  _______,  _______,  _______,  _______,    KC_NO }
               }
 
 };
@@ -88,11 +89,12 @@ void iton_bt_connection_successful() {
 bool dip_switch_update_user(uint8_t index, bool active){
   switch(index){
     case 0:
-        #ifdef BLUETOOTH_ENABLE
         if (active) {
             set_output(OUTPUT_NONE);
+            iton_bt_mode_bt();
         } else {
             set_output(OUTPUT_USB);
+            iton_bt_mode_usb();
         }
         #endif
       break;
